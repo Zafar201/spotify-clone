359cf454a097c24f12f4dd3049daa7fefef8863f
Zafar Irshad <zafarirshad201@gmail.com>
2020-09-20 14:24:29 +0530

    2:00:00

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify'
 import SpotifyWebApi from 'spotify-web-api-js'
+import Player from './Player';
+import {useDataLayerValue} from './DataLayer'
 
 const spotify = new SpotifyWebApi()
 
 function App() {
   const [token,setToken] = useState(null)
+  const [{},dispatch] = useDataLayerValue()
   //run code base on a givrn condition
-
   useEffect(()=>{
     //code...
     const hash = getTokenFromUrl()
     window.location.hash = ""
     const _token =hash.access_token
-    console.log("i have a token ",token)
+    
     if (_token){
       setToken(_token)
 
@@ -25,21 +27,15 @@ function App() {
         console.log('🤓',user)
       })
     }
-
+    console.log("i have a token ",token)
   },[])
   
   return (
-    <div className="app">
-       {
-         token ? (
-           <h1>I m logged in</h1>
-         ) : (
-          <Login/>
-         )
-       }
-       
-    </div>
-  );
+    <div className="app">{token ?
+          <Player/>:     
+          <Login/>}</div>
+           
+  )
 }
 
 export default App;
