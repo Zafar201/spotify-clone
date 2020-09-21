44e6c649a2f51b028e9b8ae6b9bf26a52b0cf42d
Zafar Irshad <zafarirshad201@gmail.com>
2020-09-21 17:39:51 +0530

    3:35:10

--- spotify-clone-app/src/reducer.js
+++ spotify-clone-app/src/reducer.js
@@ -4,7 +4,7 @@ export const initialState ={
     playing:false,
     item:null,
     //Remove after finsihed developing...
-     token:"BQBOONJgJjPlnw6zueP12T8xLKxbZ8lI0uRLVChCDSlTxyVfGodJf5W0vQoP6gKl5PbKPy5zZ4l4ebLDx1N_vb6TCPFcLih4opoFzzYOEfF25NlTVUzMFMcJVxXFadecHesTCcVMuAEt7xG5AL0Kcr9qqUSSWUtyDq88adZmxgtHQu9biuOO",
+    // token:"BQBOONJgJjPlnw6zueP12T8xLKxbZ8lI0uRLVChCDSlTxyVfGodJf5W0vQoP6gKl5PbKPy5zZ4l4ebLDx1N_vb6TCPFcLih4opoFzzYOEfF25NlTVUzMFMcJVxXFadecHesTCcVMuAEt7xG5AL0Kcr9qqUSSWUtyDq88adZmxgtHQu9biuOO",
 }
 
 const reducer =(state,action) => {
